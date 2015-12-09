7130bfe70166e80aa7b1087a4e953f8862833c7d
Jiaao Hou <jiaao.hou@yale.edu>
2015-12-09 01:17:13 -0500

    bug free (hopefully)

--- shifts_scheduler.py
+++ shifts_scheduler.py
@@ -1,96 +1,9 @@
+import pdb
 from __future__ import division
 from times import *
+from people import *
 
 times = Times(0, 10)
-
-class Population:
-  def __init__(self):
-    self.num_people = 0
-    self.people = []
-
-  def __repr__(self):
-    return "<num people: %d>" % (self.num_people)
-
-  def add_person(self, new_people):
-    self.num_people += len(new_people)
-    self.people.extend(new_people)
-
-    for new_person in new_people:
-      new_person.population = self
-
-  def sort(self):
-    return sorted(self.people, key=lambda person: person.score, reverse=True)
-
-class Person:
-  def __init__(self, name, hours_needed):
-    self.name = name
-    self.hours_needed = hours_needed
-    self.adj_hours_avail = 0
-    self.score = 0
-    self.groups = []
-    self.population = None
-
-  def __repr__(self):
-    return "<name: %s, hours_needed: %d, score: %f, num groups: %d>" % (self.name, self.hours_needed, self.score, len(self.groups))
-
-  def set_score(self):
-    self.set_adj_hours_avail()
-    if self.adj_hours_avail != 0:
-      self.score = self.hours_needed / self.adj_hours_avail
-    else:
-      self.score = 0
-
-  def set_adj_hours_avail(self):
-    self.adj_hours_avail = 0
-
-    for group in self.groups:
-      for interval in group.intervals:
-        for block in interval.blocks:
-          self.adj_hours_avail += block.num_people
-
-  def add_group(self, start, end):
-    self.groups.append(Group(start, end, self))
-
-  def delete_group(self, group):
-    self.groups.remove(group)
-
-  def slice(self, start, end):
-    for group in self.groups:
-      if start >= group.start and end <= group.end:
-        group.slice(start, end)
-        return
-    raise Exception("Not within group.")
-
-  def find_intervals_to_assign(self):
-    min_conflict = self.population.num_people + 1
-    curr_int_start = curr_int_end = 0
-    max_int_len = 0
-    interval_list = []
-
-    for group in self.groups:
-      for interval in group.intervals:
-        for block in interval.blocks:
-          if block.num_people < min_conflict:                   ## new minimum conflict
-            interval_list = []
-            min_conflict = block.num_people
-            curr_int_start = block.start
-            curr_int_end = block.end
-          elif block.num_people == min_conflict:                ## same minimum conflict
-            if block.start == curr_int_end:                       ## still part of current interval
-              curr_int_end = block.end
-            else:                                                 ## new interval
-              curr_int_len = curr_int_end - curr_int_start
-              if curr_int_len > max_int_len:       ## new maximum length
-                interval_list = [(curr_int_start, curr_int_end)]
-              elif curr_int_len == max_int_len:    ## same maximum length
-                interval_list.append((curr_int_start, curr_int_end))
-              curr_int_start = block.start
-              curr_int_end = block.end
-    interval_list.append((curr_int_start, curr_int_end))
-
-
-
-    return interval_list
 
 class Group:
   def __init__(self, start, end, person):
@@ -153,44 +66,28 @@ class Interval:
     
     # slice lines up in the beginning
     elif start == self.start:
+      # update group's start
       if self.group.start == self.start:
         self.group.start = end
       self.start = end
 
     # slice lines up at the end
     elif end == self.end:
+      # update group's end
       if self.group.end == self.end:
         self.group.end = start
       self.end = start
     else:
       raise Exception("Not within interval.")
 
+    to_remove = []
     for b in self.blocks:
-      if start >= b.start and end <= b.end:
+      if b.start >= start and b.end <= end:
         b.remove_request(self.group.person)
+        to_remove.append(b)
 
-pop = Population()
-final_schedule = Times(0, 10)
-times = Times(0, 10)
+    for b in to_remove:
+      self.remove_block(b)
 
-amelia = Person("Amelia", 2)
-amelia.add_group(0, 3)
-amelia.add_group(5, 6)
-
-jay = Person("Jay", 3)
-jay.add_group(2, 8)
-
-alex = Person("Alex", 2)
-alex.add_group(6, 10)
-
-frances = Person("Frances", 2)
-frances.add_group(1, 3)
-frances.add_group(8, 10)
-
-shona = Person("Shona", 1)
-shona.add_group(5, 7)
-
-pop.add_person([amelia, jay, alex, frances, shona])
-
-for person in pop.people:
-  person.set_score()
+  def remove_block(self, block):
+    self.blocks.remove(block)
---
+++ tests.py
@@ -0,0 +1,56 @@
+def test1():
+  pop = Population()
+  final_schedule = Times(0, 10)
+  times = Times(0, 10)
+
+  amelia = Person("Amelia", 2)
+  amelia.add_group(0, 3)
+  amelia.add_group(5, 6)
+
+  jay = Person("Jay", 3)
+  jay.add_group(2, 8)
+
+  alex = Person("Alex", 2)
+  alex.add_group(6, 10)
+
+  frances = Person("Frances", 2)
+  frances.add_group(1, 3)
+  frances.add_group(8, 10)
+
+  shona = Person("Shona", 1)
+  shona.add_group(5, 7)
+  
+  pop.add_person([a,b,c,d,e])
+
+  for person in pop.people:
+    person.set_score()
+
+  pop.sort()
+
+def test2():
+  pop = Population()
+  final_schedule = Times(0, 10)
+  times = Times(0, 10)
+
+  a = Person("A", 4)
+  a.add_group(1, 9)
+
+  b = Person("B", 2)
+  b.add_group(4, 8)
+
+  c = Person("C", 2)
+  c.add_group(1, 3)
+  c.add_group(8, 10)
+
+  d = Person("D", 1)
+  d.add_group(1, 3)
+
+  e = Person("E", 1)
+  e.add_group(8, 10)
+
+  pop.add_person([a,b,c,d,e])
+
+  for person in pop.people:
+    person.set_score()
+
+  pop.sort()
